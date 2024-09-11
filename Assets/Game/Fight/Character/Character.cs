ed32a867b324f76b3809131198c850ebfa4ba468
Khenan Perso <khenan.benanou@gmail.com>
2024-09-11 22:47:33 +0200

    [FEAT] Character

--- Assets/Game/Fight/Character/Character.cs
+++ Assets/Game/Fight/Character/Character.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Character : MonoBehaviour
+[CreateAssetMenu(fileName = "Character", menuName = "Character")]
+public class Character : ScriptableObject
 {
     [SerializeField] private string characterName;
     [SerializeField] private List<Spell> spells;
+    public List<Spell> Spells => spells;
     [SerializeField] private CharacterData data;
+    public CharacterData Data => data;
+
+    public int CurrentActionPoints => data.currentActionPoints;
 }
