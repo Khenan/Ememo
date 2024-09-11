29b6d74c57533bbf33076759a674f6a3f6c1fe10
RemyAlmar <r.almar1997@gmail.com>
2024-09-11 22:49:01 +0200

    [FEAT] Add Fist Spell
    Just create a Spell's child

--- Assets/Game/Fight/Spells/Spell.cs
+++ Assets/Game/Fight/Spells/Spell.cs
@@ -1,13 +1,32 @@
-public class Spell
+public abstract class Spell
 {
-    private string name;
-    private string description;
-    private int cost;
-    private int level;
-    private int rangeMin;
-    private int rangeMax;
-    public void Effect()
+    protected SpellData spellData;
+
+    public abstract void Effect();
+}
+public class SpellData
+{
+    private readonly string name;
+    private readonly string description;
+    private readonly int cost;
+    private readonly int level;
+    private readonly int rangeMin;
+    private readonly int rangeMax;
+
+    public string Name => name;
+    public string Description => description;
+    public int Cost => cost;
+    public int Level => level;
+    public int RangeMin => rangeMin;
+    public int RangeMax => rangeMax;
+
+    public SpellData(string name, string description, int cost, int level, int rangeMin, int rangeMax)
     {
-        
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.level = level;
+        this.rangeMin = rangeMin;
+        this.rangeMax = rangeMax;
     }
 }
