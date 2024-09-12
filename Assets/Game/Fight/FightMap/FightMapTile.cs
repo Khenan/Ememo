b5edfae5a58e1c53d7bf5393f0114f2e5e2d115f
Amaury DEZELLUS <amaury.dezellus@gmail.com>
2024-09-12 23:59:59 +0200

    [UPDATE] Change the Map folder name

---
+++ Assets/Game/Fight/FightMap/FightMapTile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class FightMapTile : MonoBehaviour
+{
+    private Vector3 position;
+    public Vector3 Position => position;
+    [SerializeField] private bool isWalkable;
+    public bool IsWalkable => isWalkable;
+    [SerializeField] private bool isOccupied;
+    public bool IsOccupied => isOccupied;
+    [SerializeField] private bool isStartTile;
+    public bool IsStartTile => isStartTile;
+    [SerializeField] private int teamId;
+    public int TeamId => teamId;
+    [SerializeField] private SpriteRenderer visualTop;
+    public SpriteRenderer VisualTop => visualTop;
+    [SerializeField] private SpriteRenderer highlight;
+    public SpriteRenderer Highlight => highlight;
+
+    public Character character;
+
+    private void Start() {
+        position = gameObject.transform.position;
+    }
+
+
+
+}
