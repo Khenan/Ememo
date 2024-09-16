e4874d4818784cd92a0e92d8a62fe1c156b2de3e
Khenan Perso <khenan.benanou@gmail.com>
2024-09-16 10:27:22 +0200

    [FEAT] Hole hide this visual on start

--- Assets/Game/Utilities/Map/MapTile.cs
+++ Assets/Game/Utilities/Map/MapTile.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public abstract class MapTile : MonoBehaviour
 {
+    public bool hideVisualsOnStart;
     public Map map;
     public int ID;
     public Vector2 MatrixPosition { get; set; }
@@ -12,7 +14,14 @@ public abstract class MapTile : MonoBehaviour
     public virtual void Start()
     {
         SetMap(GetComponentInParent<Map>());
+        if(hideVisualsOnStart) HideVisuals();
     }
+
+    private void HideVisuals()
+    {
+        if(transform.childCount > 0) transform.GetChild(0).gameObject.SetActive(false);
+    }
+
     public void SetMap(Map _map)
     {
         map = _map;
